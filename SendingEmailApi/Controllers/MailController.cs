6912dff525ed89e16ebccb3b83b1c02c901cacbb
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-07-26 14:01:47 +0200

    Create `send-email` endpoint

---
+++ SendingEmailApi/Controllers/MailController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SendingEmailApi.Models;
+using SendingEmailApi.Services;
+
+namespace SendingEmailApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class MailController : ControllerBase
+	{
+		private readonly IMailService _mailService;
+
+		public MailController(IMailService mailService)
+		{
+			_mailService = mailService;
+		}
+
+		[HttpPost("send-email")]
+		public async Task<IActionResult> SendEmailAsync([FromForm] MailRequest request)
+		{
+			try
+			{
+				await _mailService.SendEmailAsync(request);
+				return Ok("Email Send Successfully ");
+			}catch(Exception ex)
+			{
+				return BadRequest(ex.Message);
+			}
+		}
+	}
+}
